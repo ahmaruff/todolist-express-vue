5dd49f2c4b1a9adbb2845b3fe022f97a97a5af20
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-26 22:06:15 +0700

    update app.js and server definition

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,22 +3,29 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./docs/swagger');
 
 const app = express();
-const port = 3000;
 
+// Middleware
 app.use(express.json());
 
-// Swagger Docs
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// Dummy Home
-app.get('/', (req, res) => {
+// Dummy Home Route
+app.get('/api', (req, res) => {
     res.send({
-        "app_name" :"Todolist",
-        "description" : "A simple web-based Todo List application, designed to showcase fullstack development skills using Node.js"
+        "app_name": "Todolist",
+        "description": "A simple web-based Todo List application, designed to showcase fullstack development skills using Node.js"
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-    console.log(`Swagger docs at http://localhost:${port}/api-docs`);
+// Swagger Docs
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Routes
+const todoRoutes = require('./todoManagement/interface/routes/todo.routes');
+app.use('/api', todoRoutes);
+
+// Error Handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ status: 'error', code: 500, message: 'Internal Server Error', data: null });
 });
+
+module.exports = app;
