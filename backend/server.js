3fd30bcd684f3c033dd12531435fb271eba6ea01
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-27 08:28:29 +0700

    feat: add env definition

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const app = require('./src/app');
-const port = 3000;
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const apiBaseUrl = process.env.API_BASE_URL || `http://localhost:${port}/api`;
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-    console.log(`Swagger docs at http://localhost:${port}/api/docs`);
+    console.log(`Server running at ${apiBaseUrl}`);
+    console.log(`Swagger docs at ${apiBaseUrl}/docs`);
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./docs/swagger');
 const todoRoutes = require('./todoManagement/interface/routes/todo.routes');
 
-const app = express();
+// Load environment variables
+dotenv.config();
 
-// === Config ===
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:5173',
-];
+const app = express();
 
 // === Middleware ===
 app.use(express.json());
 
 // CORS Middleware
+const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS ? process.env.CORS_ALLOWED_ORIGINS.split(',') : ['http://localhost:5173'];
+
+console.log(allowedOrigins);
 app.use(cors({
   origin(origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -27,13 +28,11 @@ app.use(cors({
 }));
 
 // === Routes ===
-
-// Health check / Info route
 app.get('/api', (req, res) => {
-  res.json({
-    app_name: "Todolist",
-    description: "A simple web-based Todo List application, designed to showcase fullstack development skills using Node.js",
-  });
+    res.json({
+        app_name: "Todolist",
+        description: "A simple web-based Todo List application, designed to showcase fullstack development skills using Node.js",
+    });
 });
 
 // Swagger Docs
@@ -45,13 +44,13 @@ app.use('/api/todos', todoRoutes);
 // === Error Handling ===
 // 404
 app.use((req, res, next) => {
-  res.status(404).json({ status: 'fail', code: 404, message: 'Route Not Found', data: null });
+    res.status(404).json({ status: 'fail', code: 404, message: 'Route Not Found', data: null });
 });
 
 // General Error Handler
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ status: 'error', code: 500, message: 'Internal Server Error', data: null });
+    console.error(err.stack);
+    res.status(500).json({ status: 'error', code: 500, message: 'Internal Server Error', data: null });
 });
 
 module.exports = app;
