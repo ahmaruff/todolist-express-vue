742db2e0263ec3ebee874324fab43479f0f0b326
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-27 08:04:21 +0700

    refactor: app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,48 +1,57 @@
 const express = require('express');
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./docs/swagger');
-const cors = require('cors');
-
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:5173'
-];
+const todoRoutes = require('./todoManagement/interface/routes/todo.routes');
 
 const app = express();
 
-// Middleware
+// === Config ===
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+];
+
+// === Middleware ===
 app.use(express.json());
 
-// Menggunakan CORS middleware dengan konfigurasi beberapa origin
+// CORS Middleware
 app.use(cors({
-    origin: function(origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+  origin(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
+  }
 }));
 
-// Dummy Home Route
+// === Routes ===
+
+// Health check / Info route
 app.get('/api', (req, res) => {
-    res.send({
-        "app_name": "Todolist",
-        "description": "A simple web-based Todo List application, designed to showcase fullstack development skills using Node.js"
-    });
+  res.json({
+    app_name: "Todolist",
+    description: "A simple web-based Todo List application, designed to showcase fullstack development skills using Node.js",
+  });
 });
 
 // Swagger Docs
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Routes
-const todoRoutes = require('./todoManagement/interface/routes/todo.routes');
+// Todo Routes
 app.use('/api/todos', todoRoutes);
 
-// Error Handler
+// === Error Handling ===
+// 404
+app.use((req, res, next) => {
+  res.status(404).json({ status: 'fail', code: 404, message: 'Route Not Found', data: null });
+});
+
+// General Error Handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ status: 'error', code: 500, message: 'Internal Server Error', data: null });
+  console.error(err.stack);
+  res.status(500).json({ status: 'error', code: 500, message: 'Internal Server Error', data: null });
 });
 
 module.exports = app;
